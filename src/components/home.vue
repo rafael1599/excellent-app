777f3eb9082fc576f8decaf30b9bc6b1fa2d2c63
unknown <oscar.bravo@tismart.com>
2023-09-09 18:35:28 -0500

    home responsive completed

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home" id="home">
-    <div class="card-white">
-      <div class="card-white-left">
+    <v-row class="card-white" align="center" justify="space-between" >
+      <v-col class="card-white-left" cols="12" md="6">
         <p>Serving Rockland County and surrounding areas</p>
         <p>Your Safe Ride, Pirate-Free</p>
         <v-btn
@@ -13,24 +13,28 @@
           style="padding: 0 61px; margin-top: 10px;">
             <img :src="number">
         </v-btn>
-      </div>
-      <div class="card-white-right">
-        <img :src="taxiCar" />
-        <img :src="taxiVan" />
-      </div>
-    </div>
+      </v-col>
+      <v-col class="card-white-right" cols="12" md="6">
+        <img :src="taxi" v-if="!isMobile"/>
+        <img :src="taxiCar" v-else/>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'home',
   data: () => ({
     model: 0,
     number: require('../assets/icons/number.svg'),
-    taxiCar: require('../assets/icons/taxi-car.svg'),
-    taxiVan: require('../assets/icons/taxi-van.svg')
+    taxi: require('../assets/icons/car-and-van.svg'),
+    taxiCar: require('../assets/icons/taxi-car.svg')
   }),
+  computed: {
+    ...mapState("vuetify", ['isMobile'])
+  }
 }
 </script>
 <style lang="scss">
@@ -40,11 +44,7 @@ export default {
   padding: 32px 48px;
   border-radius: 0 0 10px 10px;
   .card-white {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
     &-left {
-      max-width: 550px;
       p{
         font-size: 43px;
         font-family: 'Judson';
@@ -55,8 +55,25 @@ export default {
     }
     &-right{
       display: flex;
+      justify-content: flex-end;
       img{
-        max-width: 426px;
+        max-width: 100%;
+      }
+    }
+  }
+}
+@media (max-width: 959px) {
+  .home{
+    .card-white{
+      flex-direction: column-reverse;
+      &-left{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+      }
+      &-right{
+        justify-content: center;
       }
     }
   }
